338c63ab1c4e403c0214edd50673c2d0fcdeadde
JoaoVitor2004 <joaovitorsouzaa28122004@gmail.com>
2026-04-09 12:31:36 -0300

    Adicionar animação de scroll suave e arrumar o formulario para ser enviado corretamente

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,13 @@ const nameInput = document.querySelector("#name")
 const email = document.querySelector("#email")
 const message = document.querySelector("#message")
 
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
+
+ScrollSmoother.create({
+    smooth: 2,
+    effects: true
+});
+
 const checkEmail = (value) => {
     const regex = new RegExp(
         /^[a-zA-Z0-9-_\.]+@+[a-zA-Z]+\.+[a-z]{3}/
@@ -28,7 +35,7 @@ const showMessageError = (error) => {
             justifyContent: "space-between",
             width: "500px",
             fontSize: "16px",
-            background:"#000",
+            background: "#000",
         },
         onClick: function () { }
     }).showToast();
@@ -38,8 +45,8 @@ form.addEventListener("submit", (ev) => {
 
     ev.preventDefault()
 
-    if (nameInput.value === "" && typeof nameInput !== "string") {
-        showMessageError("Digite apenas letras")
+    if (nameInput.value === "" || nameInput.value.match(/[0-9]/gm)) {
+        showMessageError("Digite um nome valido!")
         return
     }
 
@@ -49,10 +56,10 @@ form.addEventListener("submit", (ev) => {
     }
 
     if (message.value === "") {
-        showMessageError("Prencha este campo!")
+        showMessageError("Digite uma mensagem!")
         return
     }
 
-    ev.reset()
-    ev.submit()
+    alert("Dados enviados!")
+    form.submit()
 })
